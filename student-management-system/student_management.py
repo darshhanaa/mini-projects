2b663dba28fc546658eaf8f1dc5ec23b1bf08b93
Darshana Ghodasaini <darshanaghodasaini106@gmail.com>
2026-06-18 20:00:37 +0545

    Add delete_student function to manage students

--- student-management-system/student_management.py
+++ student-management-system/student_management.py
@@ -32,4 +32,34 @@ def search_student():
         print(students[roll])
     else:
         print("Student not found.")
+def delete_student():
+    roll = input("Enter roll number: ")
 
+    if roll in students:
+        del students[roll]
+        print("Student deleted.")
+    else:
+        print("Student not found.")
+
+while True:
+    print("\n===== Student Management System =====")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Delete Student")
+    print("5. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        add_student()
+    elif choice == "2":
+        view_students()
+    elif choice == "3":
+        search_student()
+    elif choice == "4":
+        delete_student()
+    elif choice == "5":
+        break
+    else:
+        print("Invalid choice!")
